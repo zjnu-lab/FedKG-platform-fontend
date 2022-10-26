90426363b51cd3a9cc8f1832f5b7421c8a7dd8a3
Eurusyoung1991 <jacky513@qq.com>
2022-10-26 18:39:01 +0800

    10.26

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,68 +1,80 @@
-import axios from "axios"
+import axios from "axios";
 
 class api {
     constructor() {
-        this.hostUrl = 'http://192.168.1.114'
-    }
-    //
-    //登录验证
+            this.hostUrl = "http://192.168.1.114";
+        }
+        //
+        //登录验证
     postUser(username, password) {
         return axios.post("http://192.168.1.114/login", {
-            username: username, password: password
-        })
+            username: username,
+            password: password,
+        });
     }
 
     //获取实体
     getReality(token, status) {
         return axios.get("http://192.168.1.114/user/newents", {
             headers: {
-                'Authorization': "Bearer " + token
-            }, params: {
-                "status": status
-            }
-        })
+                Authorization: "Bearer " + token,
+            },
+            params: {
+                status: status,
+            },
+        });
     }
 
     //撤销删除实体
     deleteReality(token, realityId) {
         return axios.delete("http://192.168.1.114/user/newent", {
             headers: {
-                'Authorization': "Bearer " + token
-            }, params: {
-                "newentity_id": realityId
-            }
-        })
+                Authorization: "Bearer " + token,
+            },
+            params: {
+                newentity_id: realityId,
+            },
+        });
     }
 
     //查看实体详细信息
     getRealityDetail(token, realityId) {
         return axios.get("http://192.168.1.114/user/newent", {
             headers: {
-                'Authorization': "Bearer " + token
-            }, params: {
-                "newentity_id": realityId
-            }
-        })
+                Authorization: "Bearer " + token,
+            },
+            params: {
+                newentity_id: realityId,
+            },
+        });
     }
 
     //注册
     userRegister(username, password, name, unit, email, phone = null) {
         return axios.post("http://192.168.1.114/register", {
-            username: username, password: password, name: name, phone: phone, email: email, // unit:unit
-        })
+            username: username,
+            password: password,
+            name: name,
+            phone: phone,
+            email: email, // unit:unit
+        });
     }
 
     //修改密码
     changePassword(oldPassword, newPassword, token) {
-        return axios.post("http://192.168.1.114/userinfo", {
-                password: newPassword, old_password: oldPassword, phone: "", name: ""
-            }, {headers: {'Authorization': "Bearer " + token}}
-        )
+        return axios.post(
+            "http://192.168.1.114/userinfo", {
+                password: newPassword,
+                old_password: oldPassword,
+                phone: "",
+                name: "",
+            }, { headers: { Authorization: "Bearer " + token } }
+        );
     }
 
-    // 上传新实体
+    // 上传新实体 need bearer token
     postReality(data) {
-        return axios.post(`${this.hostUrl}/user/newent`, data)
+        return axios.post(`${this.hostUrl}/user/newent`, data);
     }
 }
 
